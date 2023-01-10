b056a5327686642a2efcfb3463c4e4b61db899ee
James Angelo Te Eng Fo <jartef@Jamess-MacBook-Pro-2.local>
2023-01-09 17:33:58 -0700

    added all files needed for this repo after doing all exercises.

---
+++ requests_ver.py
@@ -0,0 +1,24 @@
+'''
+Prints out the current version of the requests library installed in the system.
+'''
+import requests
+
+# print(requests.__version__)
+
+
+
+'''
+GET the google homepage
+
+credits: aaronasterling
+profile: https://stackoverflow.com/users/376728/aaronasterling
+source: https://stackoverflow.com/questions/4302027/how-to-open-a-url-in-python
+'''
+
+print(requests.get('https://www.google.com'))
+
+
+
+
+
+
