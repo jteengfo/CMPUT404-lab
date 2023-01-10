3d106047616ae9b90d9c65a2577bdb8df565a670
James T <jamesang@ualberta.ca>
2023-01-09 18:10:31 -0700

    modified python file and changed answers in the .txt file

--- requests_ver.py
+++ requests_ver.py
@@ -3,7 +3,7 @@ Prints out the current version of the requests library installed in the system.
 '''
 import requests
 
-# print(requests.__version__)
+print(requests.__version__)
 
 
 
@@ -17,6 +17,13 @@ source: https://stackoverflow.com/questions/4302027/how-to-open-a-url-in-python
 
 print(requests.get('https://www.google.com'))
 
+
+'''
+Modify your Python script so that it downloads itself from GitHub and prints out its own source code from GitHub.
+
+'''
+
+print(requests.get("https://raw.githubusercontent.com/jteengfo/CMPUT404-lab/main/requests_ver.py").text)
 
 
 
